701e8bf5f8218de1d72dbf43687d6a755f9a827b
Yuri Brunetto <brunettoyuri@gmail.com>
2025-08-27 18:49:11 -0300

    feat: initial commit

---
+++ src/main/java/com/brunetto/encourtner/EncourtnerApplication.java
@@ -0,0 +1,13 @@
+package com.brunetto.encourtner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncourtnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncourtnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunetto/encourtner/EncourtnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunetto.encourtner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncourtnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
