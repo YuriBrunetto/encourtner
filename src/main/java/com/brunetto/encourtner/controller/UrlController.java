c99bd465eeae3e973c16d6406452bda1c3a5c774
Yuri Brunetto <brunettoyuri@gmail.com>
2025-08-27 21:15:18 -0300

    feat: add Url controller

---
+++ src/main/java/com/brunetto/encourtner/controller/UrlController.java
@@ -0,0 +1,34 @@
+package com.brunetto.encourtner.controller;
+
+import com.brunetto.encourtner.dto.UrlRequestDTO;
+import com.brunetto.encourtner.model.Url;
+import com.brunetto.encourtner.service.UrlService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UrlController {
+    private final UrlService urlService;
+
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+
+    @PostMapping("/shorten")
+    public ResponseEntity<Url> createShortUrl(@RequestBody UrlRequestDTO request) {
+        Url createdUrl = urlService.generateShortUrl(request.getUrl());
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUrl);
+    }
+
+    @GetMapping("/{shortCode}")
+    public ResponseEntity<Void> redirectToOriginalUrl(@PathVariable String shortCode) {
+        Url originalUrl = urlService.getOriginalUrl(shortCode);
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create(originalUrl.getLongUrl()))
+                .build();
+    }
+}
